cd6a517509c7b533c0fc7b960c332c77b7bd91dc
dozuolmez <dozuolmez@ymor.com>
2021-01-20 21:51:04 +0100

    20.01.2021

---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snippet
+    class Product //entity
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+        //CRUD
+
+    }
+}
